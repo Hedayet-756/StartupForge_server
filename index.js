ab0783f3a9f37b85463c33fe416e303284ccae54
Hedayet-756 <uhedayet756@gmail.com>
2026-07-28 10:20:40 +0600

    Impment founder  post api

--- index.js
+++ index.js
@@ -25,6 +25,86 @@ async function run() {
 
         const database = client.db("StartupForge_db");
         const opportunitiesCollection = database.collection("opportunities");
+        const founderCollection = database.collection("founders");
+
+        app.get('/api/opportunities', async (req, res) => {
+            try {
+                const query = {};
+                if (req.query.founderId) {
+                    query.founderId = req.query.founderId;
+                }
+                if (req.query.status) {
+                    query.status = req.query.status;
+                }
+                const cursor = opportunitiesCollection.find(query);
+                const result = await cursor.toArray();
+                res.json(result);
+            }
+            catch (error) {
+                console.error("Error fetching opportunities:", error);
+                res.status(500).json({ error: true, message: "Internal Server Error" });
+            }
+        });
+
+        // app.get('/api/jobs', async (req, res) => {
+        //     try {
+        //         const { companyId, status, jobType, category, search, isRemote, page, perPage } = req.query; // req.query থেকে ডাটাগুলো ডিপ্রাক্ট করে নেওয়া হলো
+
+        //         console.log('server side q', req.query);
+        //         const query = {};
+
+        //         // search related query
+        //         if (search && search !== 'undefined' && search !== '') {
+        //             query.$or = [
+        //                 { title: { $regex: search, $options: 'i' } },
+        //                 { companyName: { $regex: search, $options: 'i' } },
+        //                 { category: { $regex: search, $options: 'i' } }
+        //             ];
+        //         }
+
+        //         // jobType filter related query (undefined চেক সহ)
+        //         if (jobType && jobType !== 'undefined' && jobType !== '') {
+        //             query.type = jobType;
+        //         }
+
+        //         // category filter related query (undefined চেক সহ)
+        //         if (category && category !== 'undefined' && category !== '') {
+        //             query.category = category;
+        //         }
+
+        //         // isRemote filter
+        //         if (isRemote === 'true') {
+        //             query.isRemote = true;
+        //         }
+
+        //         // company related query
+        //         if (companyId && companyId !== 'undefined') {
+        //             query.companyId = companyId;
+        //         }
+        //         if (status && status !== 'undefined') {
+        //             query.status = status;
+        //         }
+
+        //         // pagination related query
+        //         if (page && page !== 'undefined') {
+        //             const pageNumber = parseInt(page);
+        //             const limitNumber = parseInt(perPage) || 12;
+        //             const skipItems = (pageNumber - 1) * limitNumber;
+
+        //             const total = await jobCollection.countDocuments(query);
+        //             const cursor = jobCollection.find(query).skip(skipItems).limit(limitNumber);
+        //             const jobs = await cursor.toArray();
+        //             return res.json({ jobs, total });
+        //         }
+
+        //         const cursor = jobCollection.find(query);
+        //         const result = await cursor.toArray();
+        //         res.json(result);
+        //     } catch (error) {
+        //         console.error("Error fetching jobs:", error);
+        //         res.status(500).json({ error: true, message: "Internal Server Error" });
+        //     }
+        // });
 
 
         app.post('/api/opportunities', async (req, res) => {
@@ -42,63 +122,19 @@ async function run() {
             }
         });
 
-        app.get('/api/jobs', async (req, res) => {
+
+        app.post('/api/founders', async (req, res) => {
             try {
-                const { companyId, status, jobType, category, search, isRemote, page, perPage } = req.query; // req.query থেকে ডাটাগুলো ডিপ্রাক্ট করে নেওয়া হলো
-
-                console.log('server side q', req.query);
-                const query = {};
-
-                // search related query
-                if (search && search !== 'undefined' && search !== '') {
-                    query.$or = [
-                        { title: { $regex: search, $options: 'i' } },
-                        { companyName: { $regex: search, $options: 'i' } },
-                        { category: { $regex: search, $options: 'i' } }
-                    ];
+                const founder = req.body;
+                const newFounder = {
+                    ...founder,
+                    createdAt: new Date(),
                 }
-
-                // jobType filter related query (undefined চেক সহ)
-                if (jobType && jobType !== 'undefined' && jobType !== '') {
-                    query.type = jobType;
-                }
-
-                // category filter related query (undefined চেক সহ)
-                if (category && category !== 'undefined' && category !== '') {
-                    query.category = category;
-                }
-
-                // isRemote filter
-                if (isRemote === 'true') {
-                    query.isRemote = true;
-                }
-
-                // company related query
-                if (companyId && companyId !== 'undefined') {
-                    query.companyId = companyId;
-                }
-                if (status && status !== 'undefined') {
-                    query.status = status;
-                }
-
-                // pagination related query
-                if (page && page !== 'undefined') {
-                    const pageNumber = parseInt(page);
-                    const limitNumber = parseInt(perPage) || 12;
-                    const skipItems = (pageNumber - 1) * limitNumber;
-
-                    const total = await jobCollection.countDocuments(query);
-                    const cursor = jobCollection.find(query).skip(skipItems).limit(limitNumber);
-                    const jobs = await cursor.toArray();
-                    return res.json({ jobs, total });
-                }
-
-                const cursor = jobCollection.find(query);
-                const result = await cursor.toArray();
-                res.json(result);
+                const result = await founderCollection.insertOne(newFounder);
+                res.send(result);
             } catch (error) {
-                console.error("Error fetching jobs:", error);
-                res.status(500).json({ error: true, message: "Internal Server Error" });
+                console.error("Database Insert Error:", error);
+                res.status(500).json({ error: true, message: "Database connection failed" });
             }
         });
 
